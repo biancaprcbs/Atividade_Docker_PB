2f3f12cf7b302a66c23255a78ecfbef39a5214b8
biancaprcbs <biancaprcbs@alu.ufc.br>
2023-10-27 16:51:56 -0300

    atualização do script user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -7,5 +7,7 @@ sudo usermod -aG docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo yum install nfs-utils -y
+sudo systemctl start nfs-server
+sudo systemctl enable nfs-server
 sudo mkdir /mnt/efs/
 sudo chmod 777 /mnt/efs/

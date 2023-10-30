7242ce6ce403ec6e5918156b36490dffc6368b4a
biancaprcbs <biancaprcbs@alu.ufc.br>
2023-10-30 13:45:39 -0300

    alterações finais no script

--- template_user_data.sh
+++ template_user_data.sh
@@ -18,8 +18,10 @@ sudo systemctl enable nfs-server
 sudo mkdir /mnt/efs/
 sudo chmod 777 /mnt/efs/
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0e395848c30fa3067.efs.us-east-1.amazonaws.com:/ /mnt/efs
+sudo chown ec2-user:ec2-user /mnt/efs
 echo "fs-0e395848c30fa3067.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" | sudo tee -a /etc/fstab
 
 # configuração do wordpress com docker-compose
 curl -sL "https://raw.githubusercontent.com/biancaprcbs/Atividade_Docker_PB/main/docker-compose.yaml" --output "/home/ec2-user/docker-compose.yaml"
+cd /home/ec2-user
 docker-compose up -d
